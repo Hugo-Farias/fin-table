b73cea8bc55143fb86e652b5543f646d87cdd353
Hugo-Farias <hg80rod@gmail.com>
2023-07-31 14:45:35 -0300

    intital commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.scss";
+import Titlebar from "./components/Titlebar";
+
+function App() {
+  return <Titlebar />;
+}
+
+export default App;
---
+++ src/components/Titlebar.tsx
@@ -0,0 +1,7 @@
+import "./Titlebar.scss";
+
+const Titlebar = function () {
+  return <h1>Test</h1>;
+};
+
+export default Titlebar;
