a72079a73a2788c948e884ec5570b44b0135a569
Hugo-Farias <hg80rod@gmail.com>
2023-08-05 15:30:20 -0300

    experimenting with primereact again

--- src/components/MainTable.tsx
+++ src/components/MainTable.tsx
@@ -3,7 +3,7 @@ import { formatDate } from "../helper";
 import { Bill } from "../types";
 import dummy from "../data/DUMMY_DATA.json";
 
-const columnCategories = [
+const categories = [
   { cName: "select", content: <input type="checkbox" /> },
   { cName: "day", content: "DIA" },
   { cName: "name", content: "NOME" },
@@ -17,13 +17,13 @@ const columnCategories = [
 const MainTable = function () {
   const products: Bill[] = dummy;
 
-  const day = products.map((v) => {
-    return v.day;
-  });
+  // const day = products.map((v) => {
+  //   return v.day;
+  // });
 
-  console.log(formatDate(day[3], ["year", "month", "day"]));
+  // console.log(formatDate(day[3], ["year", "month", "day"]));
 
-  const cNameJSX = columnCategories.map((v, i) => {
+  const cNameJSX = categories.map((v, i) => {
     return (
       <div key={i} className={`c-name ${v.cName}`}>
         {v.content}
@@ -31,9 +31,23 @@ const MainTable = function () {
     );
   });
 
+  const tableContentsJSX = categories.map((v, i) => {
+    const out = products.map((pv, pi) => {
+      const content = pv[v.cName] ? pv[v.cName] : v.content;
+
+      return (
+        <div key={pi} className={`c-name ${v.cName}`}>
+          {content}
+        </div>
+      );
+    });
+    return <div className={`c-name ${v.cName}`}>{out}</div>;
+  });
+
   return (
     <div className="main-table">
       <div className="columns">{cNameJSX}</div>
+      <div className="table-contents">{tableContentsJSX}</div>
     </div>
   );
 };
--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,3 @@
-import { dataState } from "./store/dataSlice";
-// import store from "./store";
-//
-// export type dataSliceState = ReturnType<typeof store.getState>;
-
-// export interface Product {
-//   id: string;
-//   code: string;
-//   name: string;
-//   description: string;
-//   price: number;
-//   category: string;
-//   quantity: number;
-//   inventoryStatus: string;
-//   rating: number;
-// }
-
 export interface Bill {
   id: number;
   day: number;
